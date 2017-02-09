68daca72959e1e84e55fea71c75bf3b47337b691
nevilleblackson <nblackson@roc-dev.com>
2017-02-09 08:45:36 +0100

    .

--- tourguide_Project/app/src/main/java/com/example/gebruiker/tourguide_project/WinkelActivity.java
+++ tourguide_Project/app/src/main/java/com/example/gebruiker/tourguide_project/WinkelActivity.java
@@ -3,11 +3,11 @@ package com.example.gebruiker.tourguide_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Destad extends AppCompatActivity {
+public class WinkelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_destad);
+        setContentView(R.layout.activity_winkels);
     }
 }
